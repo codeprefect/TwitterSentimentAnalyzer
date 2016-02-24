5d62f063d31d57263c3c172c4029ab7567783da6
mubarakadeimam <mubarakadeimam@yahoo.com>
2016-02-24 15:50:14 +0100

    Tweet Fetching OK

--- account_details.rb
+++ account_details.rb
@@ -13,5 +13,16 @@ module Tweeter
     def get_feed(count)
       Connection.feed_request(@username, count)
     end
+
+    # ADD CODE TO ITERATE THROUGH EACH TWEET AND PRINT ITS TEXT
+
+    def tweet_collect(count)
+      tweets_hash = get_feed(count)
+      @tweets_only = []
+      tweets_hash.each do |tweet|
+        @tweets_only << tweet['text']
+      end
+      @tweets_only
+    end
   end
 end
--- andelatsa.rb
+++ andelatsa.rb
@@ -1,6 +1,18 @@
 require_relative "helper"
 require_relative "account_details"
 
-mubarak = Tweeter::Twitter_Account.new("mubarakadeimam")
+class TweetFetcher
+  include Tweeter, Helper
+  def initialize(user, count)
+    @user = Tweeter::Twitter_Account.new(user)
+    @count = count
+  end
 
-puts mubarak.get_feed(1)["text"]
+  def tweet_store
+    tweet_file = File.open("tweets.json", "w+")
+    tweet_file << @user.tweet_collect(@count)
+  end
+end
+
+mubarak = TweetFetcher.new("amoralyn", 10)
+puts mubarak.tweet_store
